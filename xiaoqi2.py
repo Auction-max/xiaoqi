53d71769fc0d1948f27f92226fb75093788010e3
Auction-max <3491076106@qq.com>
2025-06-27 14:48:22 +0800

    Update xiaoqi2.py

--- xiaoqi2.py
+++ xiaoqi2.py
@@ -44,15 +44,6 @@ with st.sidebar:
     elif api_vendor == "DeepSeek":
         base_url = 'https://api.deepseek.com'
         model_options = [
-            "gpt-3.5-turbo",
-            "gpt-3.5-turbo-0301",
-            "gpt-3.5-turbo-0613",
-            "gpt-3.5-turbo-16k",
-            "gpt-3.5-turbo-16k-0613",
-            "gpt-4",
-            "gpt-4-0314",
-            "gpt-4-0613",
-            "gpt-4-32k",
             'deepseek-chat',
             'deepseek-reasoner',
             'deepseek-text-v1',
